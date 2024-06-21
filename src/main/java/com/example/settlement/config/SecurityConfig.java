b2856df12116a2e58b524c3dafe3284848c8f8df
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-06-22 00:57:55 +0900

    Feat: spring security 추가

---
+++ src/main/java/com/example/settlement/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.settlement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/auth/**").permitAll() // 회원가입 및 로그인 엔드포인트는 누구나 접근 가능
+                                .anyRequest().authenticated() // 그 외의 요청은 인증 필요
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .permitAll()
+                );
+        return http.build();
+    }
+}
