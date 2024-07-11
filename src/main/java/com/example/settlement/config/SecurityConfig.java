6a92abe3c1cc27046f7bfb98009a1146ee2a1b9b
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-07-12 02:55:10 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/settlement/config/SecurityConfig.java
+++ src/main/java/com/example/settlement/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.settlement.config;
 
 import com.example.settlement.security.JwtRequestFilter;
 import com.example.settlement.user.service.CustomUserDetailsService;
-import com.example.settlement.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
